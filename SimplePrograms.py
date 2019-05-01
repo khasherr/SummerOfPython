61f68c0ef3b75ccdf97ed4f49c687f8143b2a7ef
khasher <khansherr@gmail.com>
2019-05-01 16:12:19 -0400

    First Programs
    my first programs in python

---
+++ SimplePrograms.py
@@ -0,0 +1,36 @@
+#1 finds the average of 3 numbers
+print("Enter 3 numbers to find average")
+userInput1= int(input())
+userInput2 = int(input())
+userInput3 = int(input())
+average =  (userInput1 + userInput2 + userInput3) / 3
+
+print(average)
+
+print("*************************************************")
+
+#2 finds exponentials
+userInput4 = int(input())
+userInput5 = int(input())
+exponent = userInput4 ** userInput5
+print(exponent)
+
+print("*************************************************")
+
+#3 finds arithmetic progression
+userInput6 = int(input())
+userInput7 = int(input())
+userInput8 = int(input())
+arthprog = (userInput7 - userInput6)
+print(int (arthprog))
+
+print("**************************************************")
+
+
+#finds area covered by the plane by the rectangle
+userInputx1 = int(input())
+userInputy1 = int(input())
+userInputx2 = int(input())
+userInputy2 = int(input())
+areaOccupied = (userInputx2 - userInputx1) * (userInputy2 -userInputy1)
+print(areaOccupied)
