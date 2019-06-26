0b94b086b7484b45d51eb63e979244e0ee9e4325
khasher <khansherr@gmail.com>
2019-06-25 20:44:32 -0400

    Push Zero's to end

---
+++ 0'1'Sort.py
@@ -0,0 +1,19 @@
+#Sher Khan
+#This program pushes 0's at the end - bubble sort concept but ascending
+def sort(array):
+    length = len(array)
+    for i in range (length-1):
+        for j in range (length -1 -i):
+            if array[j+1] > array[j]:
+                array[j+1], array[j] = array[j], array[j+1]
+                print(array)
+
+
+# numberofinput = int(input())
+# array = [int (x) for x in input().split()]
+# # sorted = sort(array)
+# print(sorted)
+
+#test
+array = [0,0,1,3,4]
+print(sort(array))
