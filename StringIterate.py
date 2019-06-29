6b7dbf981de1635b20f81967e61bedc33d965ae2
Sher <38057955+khasherr@users.noreply.github.com>
2019-06-29 17:15:42 -0400

    Update StringIterate.py

--- StringIterate.py
+++ StringIterate.py
@@ -53,6 +53,10 @@ print(b>= "K")  #true because 1) ascii valur and length
 print(b> "KHAN") #true because lower case asci valye greater
 print("KHAN"> b) #false
 
+#interesting one : 
+a = "abce"
+print(a>= "abcdef") #will return true because 1) every characters are compared and e has has higher ascii than d so will return true
+
 #keypoint to remember 1) strings are stored on basis of ascii value 2) strings are immutable 
 
 
